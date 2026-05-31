6b69410a55004dff1a3a8284bde52d79503f5ba8
haduydai <daithan348@gmail.com>
2026-06-01 02:42:10 +0700

    feat(Issue 5):giả lập nhận mã code từ google oauth2 bằng text và hiển thị ra màn hình

--- MovieTicketBooking/src/main/java/controller/GoogleLoginServlet.java
+++ MovieTicketBooking/src/main/java/controller/GoogleLoginServlet.java
@@ -7,8 +7,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+
 
 @WebServlet("/google-login")
 public class GoogleLoginServlet extends HttpServlet {
@@ -24,19 +29,19 @@ public class GoogleLoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String code = req.getParameter("code");
-        if(code == null){
-            String authorizationUrl = GOOGLE_AUTH_URL+"?"
-                    +"client_id="+ URLEncoder.encode(CLIENT_ID, StandardCharsets.UTF_8)
-                    +"&redirect_uri="+URLEncoder.encode(REDIRECT_URI, StandardCharsets.UTF_8)
-                    +"&response_type=code"
-                    +"&scope="+URLEncoder.encode("email profile", StandardCharsets.UTF_8);
+        if (code == null) {
+            String authorizationUrl = GOOGLE_AUTH_URL + "?"
+                    + "client_id=" + URLEncoder.encode(CLIENT_ID, StandardCharsets.UTF_8)
+                    + "&redirect_uri=" + URLEncoder.encode(REDIRECT_URI, StandardCharsets.UTF_8)
+                    + "&response_type=code"
+                    + "&scope=" + URLEncoder.encode("email profile", StandardCharsets.UTF_8);
             resp.sendRedirect(authorizationUrl);
             return;
-
         }
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Đọc mã code từ Google và in thẳng ra màn hình
+        resp.setContentType("text/html;charset=UTF-8");
+        resp.getWriter().println("<h3>[Bước 2.1] Nhận mã Code thành công!</h3>");
+        resp.getWriter().println("<p>Mã Code nhận được: <b>" + code + "</b></p>");
 
     }
 }
